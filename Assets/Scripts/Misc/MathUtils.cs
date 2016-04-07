271e1cd61cad592febbdaeeb61e3387314de44c8
Jeremy Bond <jeremybond2@hotmail.com>
2016-04-07 21:44:46 +0200

    added level creation test

---
+++ Assets/Scripts/Misc/MathUtils.cs
@@ -0,0 +1,13 @@
+﻿//Created By: Jeremy Bond
+//Date: 
+
+using UnityEngine;
+
+public static class MathUtils 
+{
+
+	public static float difference (float a, float b)
+	{
+		return Mathf.Abs (a - b);
+	}
+}

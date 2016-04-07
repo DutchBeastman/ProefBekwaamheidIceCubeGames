ceb45fdbeacba26af6ef25a70ae64fefd0e18b77
Jeremy Bond <jeremybond2@hotmail.com>
2016-04-07 22:46:17 +0200

    added a follow script for the camera and following the player

---
+++ Assets/Scripts/TestScripts/FallWith.cs
@@ -0,0 +1,14 @@
+﻿//Created By: Jeremy Bond
+//Date: 07/04/2016
+
+using UnityEngine;
+
+public class FallWith : MonoBehaviour 
+{
+	[SerializeField] private GameObject objectToFallWith;
+
+	protected void Update () 
+	{
+		transform.position = new Vector3(transform.position.x, objectToFallWith.transform.position.y, 0);
+	}
+}

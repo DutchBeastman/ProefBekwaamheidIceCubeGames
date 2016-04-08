cea287fda3f30b988bf5d1897c12c51235c15265
Fabian <fabianverk@hotmail.com>
2016-04-08 12:41:46 +0200

    Node generation start

---
+++ Assets/Scripts/Generation/TileGeneration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileGeneration : MonoBehaviour {
+	private List<GameObject> blocksList;
+
+	private IEnumerator InitialGeneration()
+	{
+		for (int i = 0; i < 15; i++)
+		{
+			for (int j = 0; j < 15; j++)
+			{
+						
+			}
+		}
+		yield return new WaitForEndOfFrame();
+	}
+}

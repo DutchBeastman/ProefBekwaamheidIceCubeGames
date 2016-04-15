b7cdbdd773189481f81625694860786d735ffab7
Fabian <fabianverk@hotmail.com>
2016-04-15 16:20:07 +0200

    adding new level generation

---
+++ Assets/Scripts/Blocks/Block.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Type
+{
+	Blue = 0, Green, Pink, Yellow,
+	// Hard,
+	// Air,
+	Empty
+}
+public class Block : MonoBehaviour {
+
+	private Vector2 position;
+	public Type type;
+	public Vector2 Position
+	{
+		get
+		{
+			return position;
+		}
+		set
+		{
+			position = value;
+		}
+	}
+}
---
+++ Assets/Scripts/Blocks/BlockManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlockManager : MonoBehaviour
+{
+
+	[SerializeField]private GameObject[] tiles;
+	[SerializeField]private Dictionary<Block, Vector2> listOfBlocks;
+	private List<Vector2> directionsList = new List<Vector2>();
+	[SerializeField]private int fieldX;
+	[SerializeField]private int fieldY;
+
+	protected void Awake()
+	{
+		directionsList.Add(new Vector2(+1 , 0));
+		directionsList.Add(new Vector2(-1 , 0));
+		directionsList.Add(new Vector2(0 , -1));
+		directionsList.Add(new Vector2(0 , +1));
+		Generation();
+	}
+	private void Generation()
+	{
+		for (int x = 0; x < fieldX; x++)
+		{
+			for (int y = 0; y < fieldY; y++)
+			{
+				GameObject instatiateBlock = (GameObject)Instantiate(tiles[Random.Range(0 , tiles.Length)] , new Vector2(transform.position.x + x , transform.position.y - y ) , Quaternion.identity);
+				Block block = instatiateBlock.GetComponent<Block>();
+				//listOfBlocks.Add(block , new Vector2(x , y));
+			}
+
+		}
+	}
+	private void DetermineNeighbours(Block current)
+	{
+
+	}
+}

7ca757372a3a25163a994752e2933ecedf62c74f
Jeremy Bond <jeremybond2@hotmail.com>
2016-04-16 05:22:04 +0200

    added a level logo transition script without the fading or showing anything

---
+++ Assets/Scripts/LevelTransition/LogoTransition.cs
@@ -0,0 +1,36 @@
+﻿// Created by: Jeremy Bond
+// Date: 15/04/2016
+
+using UnityEngine;
+
+public class LogoTransition : MonoBehaviour
+{
+	[SerializeField] private Sprite[] transitionLogos;
+	private Sprite currentLogo;
+	private new SpriteRenderer renderer;
+	private int nextTransition;
+	
+	protected void Start()
+	{
+		renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	public void StartTransition()
+	{
+		currentLogo = transitionLogos[nextTransition];
+		FadeInLogo();
+		Invoke("FadeOut", 3);
+		nextTransition++;
+	}
+	
+	private void FadeInLogo()
+	{
+		renderer.sprite = currentLogo;
+		
+	}
+
+	private void FadeOutLogo()
+	{
+
+	}	
+}

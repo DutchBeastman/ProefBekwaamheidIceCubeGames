8b9d338dd3a38089aaeb137080ba7998db911d3c
Fabian Verkuijlen <FabianVerk@hotmail.com>
2016-04-18 23:49:44 +0200

    Playermovement added

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -4,13 +4,23 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-	
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private BlockManager manager;
+    private float movement = 0.04f;
+
+    protected void Update()
+    {
+        transform.Translate(Mathf.Clamp(Input.GetAxis("Horizontal"), -movement, movement), 0, 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + Vector2.down.y), Vector2.down, 0.1f);
+            Debug.DrawRay(new Vector2(transform.position.x, transform.position.y + Vector2.down.y), Vector2.down, Color.red);
+            if (hit.collider != null && hit.collider.GetComponent<Block>())
+            {
+                Debug.Log("colliders name" + hit.collider.name);
+                manager.KillTile(hit.collider.GetComponent<Block>());
+            }
+        }
+    }
 }

5fba35d0762367089ea0485833e4d2328a690425
Fabian <fabianverk@hotmail.com>
2016-04-19 16:00:02 +0200

    Player movement commented

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -7,11 +7,13 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]private BlockManager manager;
+	private bool canDig;
 	private Rigidbody2D rigid;
 
 	protected void Awake()
 	{
 		rigid = GetComponent<Rigidbody2D>();
+		canDig = true;
 	}
 	protected void FixedUpdate()
 	{
@@ -21,15 +23,27 @@ public class PlayerMovement : MonoBehaviour
 	}
     protected void Update()
     {
+		//Getting the axis for spacebar, which in unity is called jump
 		if (Input.GetButtonDown("Jump"))
         {
+			//Raycasting to see if we hit an other Block collider
             RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + Vector2.down.y), Vector2.down, 0.1f);
             Debug.DrawRay(new Vector2(transform.position.x, transform.position.y + Vector2.down.y), Vector2.down, Color.red);
-            if (hit.collider != null && hit.collider.GetComponent<Block>())
+            if (hit.collider != null && hit.collider.GetComponent<Block>() && canDig)
             {
+				//Here we remove a block, and set the digging unavailiable and start the reset timer
                 Debug.Log("colliders name" + hit.collider.name);
                 manager.KillTile(hit.collider.GetComponent<Block>());
+				canDig = false;
+				Invoke("ResetDigTime" , 0.7f);
             }
         }
     }
+	/// <summary>
+	/// Is in place for the dig funtion to be able to execute after a certain time.
+	/// </summary>
+	private void ResetDigTime()
+	{
+		canDig = true;
+	}
 }

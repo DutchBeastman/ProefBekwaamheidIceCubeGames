d3f01aa18ae5a196e1f5403bf480f556ace0833c
Jeremy Bond <jeremybond2@hotmail.com>
2016-04-20 11:55:20 +0200

    added final tile prefab

---
+++ Assets/Scripts/Blocks/FinalTile.cs
@@ -0,0 +1,17 @@
+﻿// Created by: Jeremy Bond
+// Date: 20/04/2016
+
+using UnityEngine;
+using System.Collections;
+
+public class FinalTile : MonoBehaviour
+{
+	[SerializeField] private BlockManager manager;
+	[SerializeField] private LogoTransition logo;
+
+	protected void OnDestroy()
+	{
+		logo.ShowLogo();
+		manager.Reset();
+	}
+}

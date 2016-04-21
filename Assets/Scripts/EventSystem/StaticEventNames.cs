142be191b4899b44ce1fd8e2f79b2ae75ff7e321
Jeremy Bond <jeremybond2@hotmail.com>
2016-04-21 14:16:30 +0200

    Made game flow work in game scene

---
+++ Assets/Scripts/EventSystem/StaticEventNames.cs
@@ -0,0 +1,10 @@
+﻿// Created by: Jeremy Bond
+// Date: 21/04/2016
+
+public class StaticEventNames
+{
+	public const string ENABLEMAINMENU = "EnableMainMenu";
+	public const string ENABLEGAMECREATION = "EnableGameCreation";
+	public const string ENDGAME = "EndGame";
+	public const string RESTART = "Restart";
+}
---
+++ Assets/Scripts/GameManagement/GamePartsManager.cs
@@ -0,0 +1,29 @@
+﻿// Created by: Jeremy Bond
+// Date: 21/04/2016
+
+using UnityEngine;
+
+public class GamePartsManager : MonoBehaviour
+{
+	[SerializeField] private GameObject mainMenu;
+	[SerializeField] private GameObject levelCreation;
+
+	protected void Start()
+	{
+		EventManager.AddListener(StaticEventNames.ENABLEMAINMENU, ActivateMainMenu);
+		EventManager.AddListener(StaticEventNames.ENABLEGAMECREATION, ActivateGameCreation);
+		ActivateMainMenu();
+	}
+
+	public void ActivateMainMenu()
+	{
+		levelCreation.SetActive(false);
+		mainMenu.SetActive(true);
+	}
+
+	public void ActivateGameCreation()
+	{
+		mainMenu.SetActive(false);
+		levelCreation.SetActive(true);
+	}
+}

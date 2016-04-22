75b5914cdd469827973fc51fc088ba9ead9d6aad
Fabian <fabianverk@hotmail.com>
2016-04-22 16:15:54 +0200

    Grid scripts update with instantiating a grid

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -2,7 +2,7 @@
 // Date: 21/04/2016
 
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 namespace Grid
 {
 	public class GridManager : MonoBehaviour
@@ -11,10 +11,24 @@ namespace Grid
 		private Vector2 tileSize = new Vector2(1,1);
 		[SerializeField]
 		private Vector2 gridSize = new Vector2(6, 40);
+		[SerializeField]
+		private GameObject prefab;
+		[SerializeField]
+		private List<GameObject> listOfGrid;
 
-		protected void Awake()
+        protected void Awake()
 		{
-
+			for (int i = 0; i < gridSize.x; i++)
+			{
+				for (int j = 0; j < gridSize.y; j++)
+				{
+					GameObject gridInstance = (GameObject)Instantiate(prefab ,new Vector3(tileSize.x + i,tileSize.y + -j, 0), Quaternion.identity);
+					listOfGrid.Add(gridInstance);
+					gridInstance.name = "GridInstance: " + (i+1);
+				}
+				
+			}
+			
 		}
 	}
 }

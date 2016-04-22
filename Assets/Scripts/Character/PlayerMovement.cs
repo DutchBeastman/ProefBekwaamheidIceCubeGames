acfec76966c1d494332ca3177af91c1cc1e35f86
Jeremy Bond <jeremybond2@hotmail.com>
2016-04-22 15:48:13 +0200

    removed final tile

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -37,13 +37,7 @@ public class PlayerMovement : MonoBehaviour
                 manager.KillTile(hit.collider.GetComponent<Block>());
 				canDig = false;
 				Invoke("ResetDigTime" , 0.7f);
-            }else if (hit.collider != null && hit.collider.GetComponent<FinalTile>() && canDig)
-			{
-				Debug.Log("HitFinalTile");
-				Destroy(hit.collider.gameObject);
-				canDig = false;
-				Invoke("ResetDigTime", resetDigTime);
-			}
+            }
         }
     }
 	/// <summary>

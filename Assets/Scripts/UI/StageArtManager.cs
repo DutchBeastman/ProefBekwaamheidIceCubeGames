d20c35cf201d9f71c00a43b17e3dcb23db65a0ec
Jeremy Bond <jeremybond2@hotmail.com>
2016-05-19 16:16:23 +0200

    adding stage art manager script

---
+++ Assets/Scripts/UI/StageArtManager.cs
@@ -0,0 +1,46 @@
+﻿// Created by: Jeremy Bond
+// Date: 17/05/2016
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageArtManager : MonoBehaviour 
+{
+	[SerializeField] private Sprite[] stagesImages;
+	[SerializeField] private new SpriteRenderer renderer;
+	[SerializeField] private Image image;
+	private int currentState;
+
+	protected void OnEnable ()
+	{
+		EventManager.AddListener("NextStage", NextStage);
+	}
+
+	protected void OnDisable ()
+	{
+		EventManager.RemoveListener("NextStage", NextStage);
+	}
+
+	protected void Start ()
+	{
+		SetNewSprite();
+	}
+
+	private void NextStage ()
+	{
+		currentState++;
+		SetNewSprite();
+	}
+
+	private void SetNewSprite ()
+	{
+		if (renderer)
+		{
+			renderer.sprite = stagesImages[currentState];
+		}
+		if (image)
+		{
+			image.sprite = stagesImages[currentState];
+		}
+	}
+}

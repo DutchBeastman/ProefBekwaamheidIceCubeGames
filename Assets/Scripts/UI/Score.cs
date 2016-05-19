ce95b42159f027ddb47c2a5c8ac8fccf567a0987
Jeremy Bond <jeremybond2@hotmail.com>
2016-05-19 16:16:37 +0200

    adding score script

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,53 @@
+﻿// Created by: Jeremy Bond
+// Date: 
+
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+	[HideInInspector] public int score;
+	protected void OnEnable ()
+	{
+		EventManager.AddListener ("GetPoints10", Get10Points);
+		EventManager.AddListener ("GetPoints20", Get20Points);
+		EventManager.AddListener ("GetPoints30", Get30Points);
+		EventManager.AddListener ("GetPoints50", Get50Points);
+	}
+
+	protected void OnDisable ()
+	{
+		EventManager.RemoveListener ("GetPoints10", Get10Points);
+		EventManager.RemoveListener ("GetPoints20", Get20Points);
+		EventManager.RemoveListener ("GetPoints30", Get30Points);
+		EventManager.RemoveListener ("GetPoints50", Get50Points);
+	}
+
+	private void Get10Points ()
+	{
+		score += 10;
+		UpdateUI ();
+	}
+
+	private void Get20Points ()
+	{
+		score += 20;
+		UpdateUI ();
+	}
+
+	private void Get30Points ()
+	{
+		score += 30;
+		UpdateUI ();
+	}
+
+	private void Get50Points ()
+	{
+		score += 50;
+		UpdateUI();
+	}
+
+	private void UpdateUI ()
+	{
+		Debug.Log(score);
+	}
+}

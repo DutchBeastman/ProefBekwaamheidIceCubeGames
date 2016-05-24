127a897c2fde36d5e9b8a332ec92eeeaec83e31f
Jeremy Bond <jeremybond2@hotmail.com>
2016-05-24 09:32:06 +0200

    added game over screen prefab and art

---
+++ Assets/Scripts/UI/GameOverManager.cs
@@ -0,0 +1,29 @@
+﻿// Created by: Jeremy Bond
+// Date: 24/05/2016
+
+using UnityEngine;
+using System.Collections;
+
+public class GameOverManager : MonoBehaviour 
+{
+
+	protected void Start () 
+	{
+		
+	}
+
+	protected void OnEnable ()
+	{
+		EventManager.AddListener(StaticEventNames.ENDGAME, GameOverTrigger);
+	}
+
+	protected void OnDisable ()
+	{
+		EventManager.RemoveListener (StaticEventNames.ENDGAME, GameOverTrigger);
+	}
+
+	private void GameOverTrigger ()
+	{
+
+	}
+}

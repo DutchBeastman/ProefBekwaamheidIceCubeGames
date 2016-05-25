bb53942c4fd77e6455507e30307bdac6d5c18bff
Jeremy Bond <jeremybond2@hotmail.com>
2016-05-25 10:01:32 +0200

    removing not used spine stuff

--- Assets/Scripts/Character/CharacterAnimationController.cs
+++ Assets/Scripts/Character/CharacterAnimationController.cs
@@ -2,45 +2,35 @@
 // Date: 15/04/2016
 
 using UnityEngine;
-using System.Collections;
-using Spine.Unity;
 
 public class CharacterAnimationController : MonoBehaviour
 {
 	//DigAnimations
-	[SpineAnimation, SerializeField] private string digLeftAnimation;
-	[SpineAnimation, SerializeField] private string digRightAnimation;
-	[SpineAnimation, SerializeField] private string digDownAnimation;
+	[SerializeField] private string digLeftAnimation;
+	[SerializeField] private string digRightAnimation;
+	[SerializeField] private string digDownAnimation;
 	//MoveAnimations
-	[SpineAnimation, SerializeField] private string moveLeftAnimation;
-	[SpineAnimation, SerializeField] private string moveRightAnimation;
+	[SerializeField] private string moveLeftAnimation;
+	[SerializeField] private string moveRightAnimation;
 	//FallingAnimations
-	[SpineAnimation, SerializeField] private string startFallingAnimation;
-	[SpineAnimation, SerializeField] private string fallingAnimation;
-	[SpineAnimation, SerializeField] private string stopFallingAnimation;
-
-	private SkeletonAnimation skeletonAnimation;
-
-	private Spine.AnimationState spineAnimationState;
-	//private Spine.Skeleton skeleton;
-
-
+	[SerializeField] private string startFallingAnimation;
+	[SerializeField] private string fallingAnimation;
+	[SerializeField] private string stopFallingAnimation;
+	
 	void Start()
 	{
-		skeletonAnimation = GetComponent<SkeletonAnimation>();
-		spineAnimationState = skeletonAnimation.state;
-		//skeleton = skeletonAnimation.skeleton;
+
 	}
 
 	public void MoveLeft(bool digging)
 	{
 		if (digging)
 		{
-			spineAnimationState.SetAnimation(0, digLeftAnimation, false);
+			
 		}
 		else
 		{
-			spineAnimationState.SetAnimation(0, moveLeftAnimation, false);
+
 		}
 	}
 
@@ -48,17 +38,17 @@ public class CharacterAnimationController : MonoBehaviour
 	{
 		if (digging)
 		{
-			spineAnimationState.SetAnimation(0, digRightAnimation, false);
+
 		}
 		else
 		{
-			spineAnimationState.SetAnimation(0, moveRightAnimation, false);
+
 		}
 	}
 
 	public void DigDown()
 	{
-		spineAnimationState.SetAnimation(0, digDownAnimation, false);
+
 	}
 
 	public void StartFall()

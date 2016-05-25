d5177ef3fb1d29ac2e4d763e9d64a427b1b5a6c7
Fabian <fabianverk@hotmail.com>
2016-05-25 11:40:20 +0200

    changed percentage on spawning special blocks, now smaller

--- Assets/Scripts/Blocks/BlockManager.cs
+++ Assets/Scripts/Blocks/BlockManager.cs
@@ -55,7 +55,7 @@ public class BlockManager : MonoBehaviour
 			for (int y = 0; y < fieldHeight; y++)
 			{
 				int percentageCounter = Random.Range(0 , 100);
-				if (percentageCounter < 90)
+				if (percentageCounter < 99)
 				{
 					GameObject instantiateBlock = (GameObject)Instantiate(currentStageTiles[Random.Range(0 , currentStageTiles.Length)] , new Vector2(transform.position.x + x , transform.position.y - ( fieldHeight - y )) , Quaternion.identity);
 					instantiateBlock.GetComponent<Block>().Position = new Vector2(x , y);

3e1353d47776edaab3b4d13ad59f2474d1563640
Jeremy Bond <jeremybond2@hotmail.com>
2016-05-26 09:23:38 +0200

    adding random sprite picker to the blocks

---
+++ Assets/Scripts/Blocks/SpritePicker.cs
@@ -0,0 +1,14 @@
+﻿// Created by: Jeremy Bond
+// Date: 26/05/2016
+
+using UnityEngine;
+
+public class SpritePicker : MonoBehaviour 
+{
+	[SerializeField] private Sprite[] sprites;
+
+	protected void Awake () 
+	{
+		GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0,sprites.Length)];
+	}
+}

852c4459df070246666ae04aa7861f29300aa5d5
Jeremy Bond <jeremybond2@hotmail.com>
2016-06-09 13:54:47 +0200

    fixed dying bug, the death collision objects rotated with the player, not anymore

--- Assets/Scripts/Character/DeadCollision.cs
+++ Assets/Scripts/Character/DeadCollision.cs
@@ -1,23 +1,43 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿// Created by: Fabian Verkuijlen
+// Date: 19/05/2016
+
+using UnityEngine;
 
 public class DeadCollision : MonoBehaviour {
 
 	private bool canLoseLife = true;
+
+	/// <summary>
+	/// call right side up check.
+	/// </summary>
+	protected void Update ()
+	{
+		CheckForRightSideUp();
+	}
+	/// <summary>
+	/// Checking if the right side is up.
+	/// </summary>
+	private void CheckForRightSideUp ()
+	{
+		transform.eulerAngles = Vector3.zero;
+	}
 	/// <summary>
 	/// OnTrigger in this instance makes the player lose a life
 	/// </summary>
 	/// <param name="coll">coll is the collider which the object collides with</param>
 	void OnTriggerEnter2D(Collider2D coll)
 	{
-		if (canLoseLife)
+		if (transform.eulerAngles == Vector3.zero)
 		{
-			Block b = coll.GetComponent<Block>();
-			if (b != null && b.falling && !b.pickUp)
+			if (canLoseLife)
 			{
-				canLoseLife = false;
-				EventManager.TriggerEvent(StaticEventNames.LOSTLIFE);
-				Invoke("EnableLosingLife", 4f);
+				Block b = coll.GetComponent<Block> ();
+				if (b != null && b.falling && !b.pickUp)
+				{
+					canLoseLife = false;
+					EventManager.TriggerEvent (StaticEventNames.LOSTLIFE);
+					Invoke ("EnableLosingLife", 4f);
+				}
 			}
 		}
 	}
